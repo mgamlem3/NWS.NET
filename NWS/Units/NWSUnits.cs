84f34ad5bec7ac7f93b956dd03d034a7a2bc10d9
Michael Gamlem III <mgamlem3@gmail.com>
2023-02-13 17:24:54 -0800

    Don't format unit before parsing

--- NWS/Units/NWSUnits.cs
+++ NWS/Units/NWSUnits.cs
@@ -10,27 +10,26 @@ public static class NWSUnits
 		if (unit.IsNullOrWhitespace())
 			return null;
 
-		// Allow because unit descriptions 
-#pragma warning disable CA1308
-		var formattedUnit = unit.ToLowerInvariant();
-#pragma warning restore CA1308
-		if (formattedUnit.Contains(c_celcius))
+		if (unit.Contains(c_celcius))
 			return Temperature.Celcius;
-		if (formattedUnit.Contains(c_kilometersPerHour))
+		if (unit.Contains(c_farenheit))
+			return Temperature.Farenheit;
+		if (unit.Contains(c_kilometersPerHour))
 			return Velocity.KilometersPerHour;
-		if (formattedUnit.Contains(c_pascal))
+		if (unit.Contains(c_pascal))
 			return Pressure.Pascal;
-		if (formattedUnit.Contains(c_meter))
+		if (unit.Contains(c_meter))
 			return Length.Meters;
-		if (formattedUnit.Contains(c_milimeter))
+		if (unit.Contains(c_milimeter))
 			return Length.Milimeters;
-		if (formattedUnit.Contains(c_angle))
+		if (unit.Contains(c_angle))
 			return WindDirection.Degrees;
 
 		return null;
 	}
 
 	private const string c_celcius = "degC";
+	private const string c_farenheit = "F";
 	private const string c_angle = "degree_(angle)";
 	private const string c_kilometersPerHour = "km_h-1";
 	private const string c_pascal = "Pa";
