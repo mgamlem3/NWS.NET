5dc53cb9f2880c6d5d06faac0ee06b74b02918e5
Michael Gamlem III <mgamlem3@gmail.com>
2023-02-13 16:43:08 -0800

    Use Elevation

--- NWS/Dtos/Components/Common/CloudLayer.cs
+++ NWS/Dtos/Components/Common/CloudLayer.cs
@@ -2,7 +2,7 @@ namespace NWS.Dtos.Components.Common;
 
 public sealed record CloudLayer
 {
-	public UnitWithValue? Base { get; set; }
+	public Elevation? Base { get; set; }
 	public string? Amount { get; set; }
 }
 
