90c625c42a5a374273d32ee5239c2ef0e119cf57
Michael Gamlem III <mgamlem3@gmail.com>
2023-02-13 17:13:53 -0800

    Make properties nullable

--- NWS/Dtos/Components/Common/Elevation.cs
+++ NWS/Dtos/Components/Common/Elevation.cs
@@ -2,6 +2,6 @@ namespace NWS.Dtos.Components.Common;
 
 public sealed class Elevation
 {
-	public string UnitCode { get; set; }
-	public double Value { get; set; }
+	public string? UnitCode { get; set; }
+	public double? Value { get; set; }
 }
